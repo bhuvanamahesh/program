83a1064a590896b60adbd7b736c13c72a1d7affb
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-20 02:48:25 -0800

    count.c

---
+++ count.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,c=0;
+    clrscr();
+    scanf("%d",&n);
+    while(n)
+    {
+        n=n/10;
+        c++;
+    }
+    printf("%d",c);
+
+    return 0;
+}
