15b79a9d7503b0b5d122fef89a53a94e18d7faa6
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-21 08:38:29 -0800

    armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,n1,rem,r=0;
+	scanf("%d",&n);
+	n1=n;
+	while(n1!=0)
+	{
+		rem=n1%10;
+		r=r+rem*rem*rem;
+		n1/=10;
+	}
+	if(r==n)
+	{
+		printf("armstrong");
+	}
+	else
+	{
+		printf("not a armstrong");
+	}
+	return 0;
+}
