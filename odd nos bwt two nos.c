50dc5a905596b813d75409b180770c8f0a7c5df2
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-21 07:49:57 -0800

    odd nos bwt two nos.c

---
+++ odd nos bwt two nos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int i,m,n,c=0;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+          printf("%d",i);
+            
+        }
+    }    
+        
+    return 0;
+}
+
