e89e3121223fb38368224da6027e8dd4374f80f6
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-26 09:16:45 -0800

    space count.c

---
+++ space count.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100],i,c=0;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
