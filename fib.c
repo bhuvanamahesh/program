4c1ffe6465971f768e79e611740a9d5b7b7d5057
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-26 23:39:10 -0800

    fib.c

---
+++ fib.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b,c,i;
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    printf("%d%d",a,b);
+    for(i=1;i<=n;i++)
+    {
+      c=a+b;
+      a=b;
+      b=c;
+      printf("%d",c);
+    }
+    return 0;
+}
