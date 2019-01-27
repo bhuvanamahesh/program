2ebec4af4a12a380e12bd02dc5a6fcebfc5f1bf2
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-27 00:00:07 -0800

    strktimes.c

---
+++ strktimes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[50];
+    int k,i;
+    scanf("%s",&a);
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        printf("%s\n",a);
+    }
+    
+
+    return 0;
+}
+
