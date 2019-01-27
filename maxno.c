82a8f45a6bf6103eae8f13227cfad75c85746cf8
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-26 23:02:06 -0800

    maxno.c

---
+++ maxno.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[10],i,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[1];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
