5e2c3593e5f0102b959449340dbac149f6b67066
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-27 01:33:55 -0800

    powers.c

---
+++ powers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+    printf(" %d is power of 2",n);
+    }
+    else
+    {
+    printf("not a power of two");
+    }
+
+    return 0;
+}
