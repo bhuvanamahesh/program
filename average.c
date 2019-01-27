4e21d335df0716adaef61a5445a7635be5fa9d98
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-01-27 01:27:36 -0800

    average.c

---
+++ average.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int n,a[50],sum=0,avg,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("avg is");
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+avg=sum/n;
+printf("%d",avg);
+
+    return 0;
+}
