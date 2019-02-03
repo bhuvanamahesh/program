5000f057eaf04d91acd57cc2a8ef65fdf987142d
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-03 06:09:01 -0800

    evensno.c

---
+++ evensno.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+
+int main()
+{
+    int n,a,b;
+    scanf("%d%d",&a,&b);
+    n=a*b;
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+    return 0;
+}
