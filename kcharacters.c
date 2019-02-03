761c4deedfcf82b696a7aa648ac93a64a7d9c2d5
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-03 07:02:04 -0800

    kcharacters.c

---
+++ kcharacters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[50];
+    int i,n;
+    gets(a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+
+    return 0;
+}
