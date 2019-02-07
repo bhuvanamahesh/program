bc801b26a39b40a0b860c28f7e1351c55f175d40
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-07 07:13:33 -0800

    1234.c

---
+++ 1234.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b;
+    scanf("%d%d%d",&n,&a,&b);
+    if((a<n)&&(n<b))
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+        
+    }
+        return 0;
+    
+}
+
