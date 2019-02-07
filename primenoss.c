dbf5f7ca6d9de0b6233d50bb37f9585b519dca36
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-07 06:12:38 -0800

    primenoss.c

---
+++ primenoss.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,i,count=0,n;
+	scanf("%d %d",&a,&n);
+	for(i=1;i<=n;i++)
+	{
+	if(a%i==0)
+	count++;
+	}
+	if(count==2)
+	printf("prime number");
+	else
+	printf("not prime number");
+
+
+    return 0;
+}
