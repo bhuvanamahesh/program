e24274b2b74304ba788a9e9baae22007c81a76f0
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-08 06:53:03 -0800

    arithmetics.c

---
+++ arithmetics.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d %d",&a,&b);
+    printf("\n%d%d",a,b);
+    scanf("%d %d",&c,&d);
+    printf("\n%d%d",c,d);
+    scanf("%d %d",&e,&f);
+    printf("\n%d%d",e,f);
+    printf("\n%d",(a/b));
+    printf("\n%d",(c%d));
+    printf("\n%d",(e/f));
+    
+    return 0;
+}
+
