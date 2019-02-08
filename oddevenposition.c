4523cec9c1ecea46732dc20205af5af6d441cf38
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-08 07:00:22 -0800

    oddevenposition.c

---
+++ oddevenposition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	char a[56];
+	int i,l,b;
+	scanf("%s",a);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{           b=i+1;
+		if(b%2==1)
+		printf("%c",a[i]);
+	}
+	printf("\n");
+	for(i=0;i<l;i++)
+	{
+		b=i+1;
+	if(b%2==0)
+	
+	printf("%c",a[i]);
+	}
+	return 0;
+}
