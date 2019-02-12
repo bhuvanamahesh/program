44c910228dbd375ce4e580ed37268b063fe3b839
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-12 08:20:45 -0800

    kth smallest.c

---
+++ kth smallest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+	int b,a,c,d;
+	int B,D,A,S;
+	scanf("%d",&b);
+	scanf("%d",&a);
+	scanf("%d",&c);
+	B=b-1;
+	D=N*c;
+	A=2*a;
+	S=A+D;
+	d=S*b/2;
+	printf("\n%d",d);
+  return 0;
+}
