a49bc257e2403307eaf405243e79505929f47bdd
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-12 07:38:15 -0800

    Stringchar.c

---
+++ Stringchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a[50];
+	int i,n;
+	scanf("%s %d",a,&n);
+	int len;
+	len=strlen(a);
+	for(i=n;i<=len;i++)
+	{
+		printf("%c",a[i]);
+	}
+
+	return 0;
+}
