cabf9b58a6257b18c7ad97ee71211089425e22f3
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-20 16:05:16 +0530

    leaveday.c

---
+++ leaveday.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[50];
+    int i;
+    scanf("%s",a);
+    if((a[i]=='s')||(a[i]=='S'))
+    printf("Yes");
+    else
+    printf("no");
+    return 0;
+}
