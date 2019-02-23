218a76c4ea47b4e359936e75108ab41666394521
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-23 01:58:12 -0800

    play3space.c

---
+++ play3space.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   char a[100];
+   int i,n,f=0;
+   gets(s);
+   n=strlen(s);
+   for(i=0;i<n;i++)
+   {
+       if(a[i]==' ')
+       {
+           
+       a[i]=a[i+1];
+       
+       printf("%c",a[i]);
+       
+       }
+       else
+       printf("%c",a[i]);
+   }
+    return 0;
+}
