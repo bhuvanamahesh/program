39cca1d2e2f5aa18b452fe7add878e633f935642
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-23 01:36:45 -0800

    play3sameline.c

---
+++ play3sameline.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2],b[2],c[2],i,f=0;
+	for(i=0;i<2;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	    scanf("%d",&b[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	    scanf("%d",&c[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	    if(a[i]==b[i] && b[i]==c[i])
+	    {
+	        f=1;
+	    }
+	}
+	if(f==1)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	    printf("no");
+	}
+    return 0;
+}
