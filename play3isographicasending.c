e700b7c548f1aab451b88fcc82ea20006250b8cd
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-02-23 01:54:14 -0800

    play3isographicasending.c

---
+++ play3isographicasending.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int i,j,a,n,num[i];
+scanf("%d",&n);
+scanf("%d",&num[i]);
+for(i=0;i<n;++i)
+{
+	for(j=0;j<n;++j)
+	{
+		if(num[i]>num[j])
+		{
+			a=num[i];
+			num[i]=num[j];
+			num[j]=a;
+		}
+		for(i=0;i<n;++i)
+		{
+			scanf("%d",&num);
+		}
+	}
+
+return 0;
+}
+}
