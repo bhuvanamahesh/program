45034377e6a74abb864012f3c382ba4f40afee7f
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-09 07:43:01 -0800

    play7 69.c

---
+++ play7 69.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,k,a[n],i;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-k;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
