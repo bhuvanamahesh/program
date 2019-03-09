76b9fd65114c2adfa9a09203d2e99f923d92e029
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-09 07:28:58 -0800

    play7 64.c

---
+++ play7 64.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n,c,a[n],i,j,temp;;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}	
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]<c)
+		{
+			printf("%d ",a[i]);
+		}
+	}
+	return 0;
+}
