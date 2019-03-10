965d6c689c5c0326b621a9ccd19a978e585dc8d2
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-10 05:47:53 -0700

    play8 72.c
    maximun element

---
+++ play8 72.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,a[1000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            printf("%d",a[i]);
+            break;
+        }
+        
+    }
+    return 0;
+}
