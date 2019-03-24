f12f4d2fd0a86146ffcca4a990a530ac6eae1397
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-24 04:59:53 -0700

    Update play17 166.c

--- play17 166.c
+++ play17 166.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,f=1,gcd,i;
+    int a,b,flag=1,gcd,i;
     scanf("%d %d",&a,&b);
     for(i=1;i<=a;i++)
     {
-        f=f*i;
+        flag=flag*i;
     }
-    a=f;
-    f=1;
+    a=flag;
+    flag=1;
     for(i=1;i<=b;i++)
     {
-        f=f*i;
+        flag=flag*i;
     }
-    b=f;
+    b=flag;
     for(i=1;i<=a && i<=b;i++)
     {
         if(a%i==0 && b%i==0)
