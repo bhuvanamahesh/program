2aacb4be9547871c52567a0840a82fb3911e1830
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-24 04:38:20 -0700

    Create play16 160.c

---
+++ play16 160.c
@@ -0,0 +1,19 @@
+#include<stdio.h>    
+int main()
+{  
+int a[10],n,i,s,t,c=0;    
+scanf("%d %d",&z,&y);  
+n=s|t;
+for(i=0;n>0;i++)    
+{    
+a[i]=n%2;    
+n=n/2;    
+}    
+for(i=i-1;i>=0;i--)    
+{    
+
+if(a[i]==1)
+c++;
+} 
+printf("%d",c);
+}  
