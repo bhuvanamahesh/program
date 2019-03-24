0b19dd3369419f60f7b6fbd650c1a75b772b8329
bhuvanamahesh <46838686+bhuvanamahesh@users.noreply.github.com>
2019-03-24 04:34:39 -0700

    Create play16 158.c

---
+++ play16 158.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,i=0,rem,a1[100],j,sum=0,n=0;
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    while(c)
+    {
+        rem=c%2;
+        c=c/2;
+        a1[i]=r;
+        i++;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        sum++;
+        if(a1[j]==1)
+        {
+            n++;
+        }
+        if(n==2)
+        {
+            printf("%ld",sum);
+            break;
+        }
+    }
+    return 0;
+}
